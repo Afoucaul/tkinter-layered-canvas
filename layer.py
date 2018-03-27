b82dfd74e0f3477201cc1065f614cff33b1c4000
armand <armand.foucault@telecom-bretagne.eu>
2018-03-27 04:21:04 +0200

    Fixed item overwriting

--- layer.py
+++ layer.py
@@ -2,37 +2,46 @@ import numpy as np
 
 
 class GriddedCanvasLayer:
-    def __init__(self, manager, canvas, width, height, name="Default"):
+    def __init__(self,
+                 manager, canvas, width, height, name="Default", elements=None):
         self.manager = manager
         self.canvas = canvas
-        self.elements = np.zeros((width, height), dtype=np.int16)
         self.name = name
+        if elements is not None:
+            self.elements = elements
+        else:
+            self.elements = -np.ones((width, height), dtype=np.int16)
+
+        self.visible = True
 
     def execute_draw(self, x, y, canvasMethodName: str, *args, **kwargs):
-        if self[x, y]:
-            self.delete((x, y))
+        if self[x, y] != -1:
+            self.canvas.delete(self[x, y])
+            print("Deleted item {}".format(self[x, y]))
         callback = getattr(self.canvas, canvasMethodName)
         self[x, y] = callback(*args, **kwargs, tag="layer_{}".format(self.name))
         self.manager.update_z()
 
     def clear(self, at=None):
         if at is not None:
-            if self.elements[at]:
+            if self.elements[at] != -1:
                 self.canvas.delete(self.elements[at])
-                self.elements[at] = 0
+                self.elements[at] = -1
 
         else:
-            width, height = self.elements.shape
-            for x, y in zip(range(width), range(height)):
-                self.clear((x, y))
+            for element in self.elements.flat:
+                self.canvas.delete(element)
+            self.elements[:] = -1
 
     def hide(self):
         for element in self.elements.flat:
             self.canvas.itemconfigure(element, state="hidden")
+        self.visible = False
 
     def show(self):
         for element in self.elements.flat:
             self.canvas.itemconfigure(element, state="normal")
+        self.visible = True
 
     def configure_item(self, x, y, **kwargs):
         self.canvas.itemconfig(self[x, y], **kwargs)
@@ -46,3 +55,6 @@ class GriddedCanvasLayer:
 
     def __setitem__(self, key, value):
         self.elements[key] = value
+
+    def __repr__(self):
+        return self.name
